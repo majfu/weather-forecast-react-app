81bad213834625ae5acd989c7a8c18d358a2ad3a
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2025-10-24 23:05:57 +0200

    Field can pass user input to parent component now

--- weather-app/src/components/InputField/InputField.tsx
+++ weather-app/src/components/InputField/InputField.tsx
@@ -1,12 +1,20 @@
 interface InputFieldProps {
   placeholderText: string;
+  userInput: string;
+  setUserInput: React.Dispatch<React.SetStateAction<string>>;
 }
 
-function InputField({ placeholderText }: InputFieldProps) {
+function InputField({
+  placeholderText,
+  userInput,
+  setUserInput,
+}: InputFieldProps) {
   return (
     <input
-      className="bg-white w-200 h-30 text-4xl text-gray-400 p-5 rounded-3xl border-3 border-sky-950 text-center mb-15 font-mono"
+      className="bg-white w-200 h-30 text-4xl placeholder-gray-400 text-sky-950 p-5 rounded-3xl border-6 border-sky-950 text-center mb-15 font-mono hover:cursor-pointer hover:border-sky-900"
       placeholder={placeholderText}
+      value={userInput}
+      onChange={(e) => setUserInput(e.target.value)}
     ></input>
   );
 }
