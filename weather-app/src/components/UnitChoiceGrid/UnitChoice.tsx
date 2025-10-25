223b639c8aa3a733c37d06430818737065911705
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2025-10-25 12:09:13 +0200

    Setting unit preference in local storage

--- weather-app/src/components/UnitChoiceGrid/UnitChoice.tsx
+++ weather-app/src/components/UnitChoiceGrid/UnitChoice.tsx
@@ -1,17 +1,25 @@
 interface UnitChoiceProps {
   text: string;
-  style: string;
+  value: string;
+  unitPreference: string;
+  setUnitPreference: React.Dispatch<React.SetStateAction<string>>;
 }
 
-function UnitChoice({ text, style }: UnitChoiceProps) {
-  const styles = {
-    selected: "text-white bg-sky-950",
-    notSelected: "text-sky-950 bg-sky-200 hover:text-white",
-  };
+function UnitChoice({
+  text,
+  value,
+  unitPreference,
+  setUnitPreference,
+}: UnitChoiceProps) {
+  const style =
+    value == unitPreference
+      ? "text-white bg-sky-950"
+      : "text-sky-950 bg-sky-200 hover:text-white";
 
   return (
     <div
-      className={`${styles[style]} hover:bg-sky-900 flex items-center justify-center w-20 h-16 rounded-lg cursor-pointer`}
+      onClick={() => setUnitPreference(value)}
+      className={`${style} hover:bg-sky-900 flex items-center justify-center w-20 h-16 rounded-lg cursor-pointer`}
     >
       <div>{text}</div>
     </div>
--- weather-app/src/components/UnitChoiceGrid/UnitChoiceGrid.tsx
+++ weather-app/src/components/UnitChoiceGrid/UnitChoiceGrid.tsx
@@ -1,11 +1,35 @@
+import { useState, useEffect } from "react";
 import UnitChoice from "./UnitChoice";
 
 function UnitChoiceGrid() {
+  const [unitPreference, setUnitPreference] = useState(() => {
+    return localStorage.getItem("unitPreference") || "metric";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("unitPreference", unitPreference);
+  });
+
   return (
     <div className="flex justify-end w-full mb-20 gap-10 text-4xl font-mono font-extrabold">
-      <UnitChoice text={"\u00B0C"} style="selected" />
-      <UnitChoice text={"\u00B0F"} style="notSelected" />
-      <UnitChoice text={"\u00B0K"} style="notSelected" />
+      <UnitChoice
+        text={"\u00B0C"}
+        value="metric"
+        unitPreference={unitPreference}
+        setUnitPreference={setUnitPreference}
+      />
+      <UnitChoice
+        text={"\u00B0F"}
+        value="imperial"
+        unitPreference={unitPreference}
+        setUnitPreference={setUnitPreference}
+      />
+      <UnitChoice
+        text={"\u00B0K"}
+        value="default"
+        unitPreference={unitPreference}
+        setUnitPreference={setUnitPreference}
+      />
     </div>
   );
 }
